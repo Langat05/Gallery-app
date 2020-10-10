3e2f88014e70e3e5a7dc3325dc454c139c1bc159
langat justus <justuslangat78@gmail.com>
2020-10-10 18:14:01 +0300

    added the paths to the urls file

--- gallery/urls.py
+++ gallery/urls.py
@@ -1,9 +1,19 @@
 from django.urls import path 
 from . import views
+from django.conf import settings
+from django.conf.urls.static import static
+from django.gallery.static import serve
 
 
 urlpatterns = [
     path('', views.home),
     path('about/', views.about),
     path('images/', views.images),
+    path('search/', views.search),
+    path('media/', serve,{'document_root': settings.MEDIA_ROOT}),
+    path('static/', serve,{'document_root': settings.STATIC_ROOT}),
+    
 ]
+
+if settings.DEBUG:
+    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
