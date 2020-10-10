1e7ae7a0af5d966baeef917a906e2bcc63c182f0
langat justus <justuslangat78@gmail.com>
2020-10-10 17:00:21 +0300

    created a function that search for the images

--- gallery/views.py
+++ gallery/views.py
@@ -16,4 +16,16 @@ def images(request):
     location = Location.objects.all()
     return  render(request, 'all/images.html',{'images':images}) 
 
+def search_category(request):
+    if 'image' in request.GET and request.GET["image"]:
+        search_term = request.GET.get("image")
+        searched_images = Image.search_by_category(search_term)
+        message = f"{search_term}"
+
+        return render(request, 'category.html', {"message": message, "images": searched_images})
+
+    else:
+        message = "You haven't searched for any term"
+        return render(request, 'category.html', {"message": message})    
+
     
