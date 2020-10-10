b3ab71b45a35a0c7b9668be006db6ff65d4b0e5f
langat justus <justuslangat78@gmail.com>
2020-10-10 12:02:34 +0300

    added a photo to the database

--- gallery/models.py
+++ gallery/models.py
@@ -7,9 +7,10 @@ class Image(models.Model):
     description = models.CharField(max_length=500, null=True)
     image_location = models.ForeignKey('Location',default='', on_delete = models.CASCADE)
     image_category = models.ForeignKey('Category',default='', on_delete = models.CASCADE)
+    date_created = models.DateTimeField(auto_now_add=True, null = True)
 
     def __str__(self):
-        self.save()
+        return self.name
 
 
 class Location(models.Model):
@@ -24,3 +25,4 @@ class Category(models.Model):
 
     def __str__(self):
         return self.category
+
