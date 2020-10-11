07d0f0138c5798552fb4d0ce914649bd7809bca4
langat justus <justuslangat78@gmail.com>
2020-10-11 17:23:44 +0300

    fixed  bugs on images file

--- gallery/views.py
+++ gallery/views.py
@@ -12,15 +12,15 @@ def about(request):
     return  render(request, 'all/about.html')  
 
 def images(request):
-    db_images = Image.objects.all()
+    images = Image.objects.all()
     categories = Category.objects.all()
     location = Location.objects.all()
-    return  render(request, 'all/images.html',{'images':db_images}) 
+    return  render(request, 'all/images.html',{'images':images}) 
 
 def search_category(request):
     if 'image' in request.GET and request.GET["image"]:
         search_term = request.GET.get("image")
-        searched_images = Image.search_by_category(search_term)
+        searched_images = Image.search_by_image_category(search_term)
         message = f"{search_term}"
 
         return render(request, 'category.html', {"message": message, "images": searched_images})
