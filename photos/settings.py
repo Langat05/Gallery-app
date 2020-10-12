e8a8252be1117f65b280771d79cb0d43fb08f01e
langat justus <justuslangat78@gmail.com>
2020-10-12 17:38:27 +0300

    create a media directory

--- photos/settings.py
+++ photos/settings.py
@@ -131,7 +131,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 
-MEDIA_URL = '/images/'
+MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
